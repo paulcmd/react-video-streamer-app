e3311382152884fbd3d7e8e25a571ddfc8fc196c
Paul <choxmail.pm@gmail.com>
2021-06-22 13:29:15 -0700

    moved header into browserRouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import StreamList from './streams/StreamList'
 import StreamCreate from './streams/StreamCreate'
@@ -8,26 +8,25 @@ import StreamEdit from './streams/StreamEdit'
 import StreamShow from './streams/StreamShow'
 import Header from './Header'
 
-
-
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/stream/new" exact component={StreamCreate} />
-          <Route path="/stream/edit" exact component={StreamEdit} />
-          <Route path="/stream/delete" exact component={StreamDelete} />
-          <Route path="/stream/show" exact component={StreamShow} />
-          
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/stream/new" exact component={StreamCreate} />
+                    <Route path="/stream/edit" exact component={StreamEdit} />
+                    <Route
+                        path="/stream/delete"
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path="/stream/show" exact component={StreamShow} />
+                </div>
+            </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+    )
+}
 
-
-
-export default App;
+export default App
