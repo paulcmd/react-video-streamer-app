a9e5fc838143866b9d742e68e55a1893ad8bbcd4
Paul <choxmail.pm@gmail.com>
2021-06-22 06:28:32 -0700

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+const pageOne = () => {
+  return <div>pageOne</div>;
+};
+
+const pageTwo = () => {
+  return <div>pageTwo</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={pageOne} />
+          <Route path="/2" component={pageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+
+
+
+export default App;
