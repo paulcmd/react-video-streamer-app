a515f10d34cf353d6bebb05f5433d8a03836cb02
Paul <choxmail.pm@gmail.com>
2021-06-26 23:04:24 -0700

    get user's sign in status from google api object

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class GoogleAuth extends React.Component {
+
+    state = { isSignedIn: null }
+    
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '331603116082-vldkb06nb0kqfv11dars57hhiot4mumr.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+            })
+        })
+    }
+    render() {
+        return <div>Google Auth</div>
+    }
+}
+
+export default GoogleAuth
+
+
+
+/*
+- window.gapi - we have to indicate its for the window scope
+- we are loading up the client portion of the library every time the app loads the first time
+- the callback function ie window.gapi.client... is called after this clients auth2 library is loaded into gapi
+- scope...we only need to access the user's email
+- this.auth can be referenced from anywhere in the class to figure out if user is signed in or not
+- line 12...we are updating component level state with users sign in status. it changes when users signs in or off
+*/
