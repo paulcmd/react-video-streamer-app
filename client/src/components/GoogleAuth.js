03623a81c2e105ea6c54e569aa17ba2eab4b57b4
Paul <choxmail.pm@gmail.com>
2021-06-26 23:14:11 -0700

    render sign in status to screen

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,28 +1,51 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
-
     state = { isSignedIn: null }
-    
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-                clientId: '331603116082-vldkb06nb0kqfv11dars57hhiot4mumr.apps.googleusercontent.com',
-                scope: 'email'
-            }).then(() => {
-                this.auth = window.gapi.auth2.getAuthInstance()
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
-            })
+            window.gapi.client
+                .init({
+                    clientId:
+                        '331603116082-vldkb06nb0kqfv11dars57hhiot4mumr.apps.googleusercontent.com',
+                    scope: 'email'
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance()
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                })
         })
     }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return (
+                <div>
+                    <p>I dont know if we are signed In</p>
+                </div>
+            )
+        } else if (this.state.isSignedIn) {
+            return (
+                <div>
+                    <p>I am signed In!</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>I am not Signed In</p>
+                </div>
+            )
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
 
 export default GoogleAuth
-
-
 
 /*
 - window.gapi - we have to indicate its for the window scope
