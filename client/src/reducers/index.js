144ce8656231705f44b607895c4b3dc651a5184b
Paul <choxmail.pm@gmail.com>
2021-07-08 01:04:00 -0700

    added form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer} from 'redux-form'
 import authReducer from './authReducer'
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
+
+
+
+/*
+reducer is a named export from the redux form lib. we can instead change it to 
+formReducer using 'as' so the name is more descriptive
+*/
