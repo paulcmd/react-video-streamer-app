4bfc93da1bb7054e2ba87a70666c5cd911ebf76a
Paul <choxmail.pm@gmail.com>
2021-07-08 02:47:33 -0700

    added labels to input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,18 +2,31 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
-    renderInput({ input }) {
+    renderInput({ input, label }) {
         console.log(input)
-        return <input {...input} />
+        return (
+            <div>
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
     }
 
     render() {
         console.log(this.props)
         return (
             <form>
-                <Field name="title" component={this.renderInput} />
+                <Field
+                    name="title"
+                    component={this.renderInput}
+                    label="Enter a Title"
+                />
 
-                <Field name="description" component={this.renderInput} />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label="Enter a description"
+                />
             </form>
         )
     }
