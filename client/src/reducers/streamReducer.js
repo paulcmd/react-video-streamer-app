e479a4a56d51cea33c86421527c84bcb0fdbcdb8
Paul <choxmail.pm@gmail.com>
2021-07-09 22:41:32 -0700

    Update streamReducer.js

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -10,15 +10,18 @@ export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload }
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
         default:
             return state
     }
 }
 
-
-
-
 /*
 ...state returns a new state object from state. redux requires that a new state
-object be returned
+object be returned. 
+state does not have all key value pairs in the db. it only has what we have fetched
+so far using get methods
 */
