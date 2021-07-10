2de064fc4b352b5ddc089be5d127b0bd1a1e279f
Paul <choxmail.pm@gmail.com>
2021-07-09 20:05:50 -0700

    dispatching response.data

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import { SIGN_IN, SIGN_OUT } from './types'
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types'
 import streams from '../apis/streams'
 
 export const signIn = (userId) => {
@@ -18,4 +18,8 @@ export const signOut = () => {
 export const createStream = formValues => async dispatch => {
     const response = await streams.post('/streams', formValues)
     console.log(response.data)
+    dispatch({
+        type: CREATE_STREAM,
+        payload: response.data
+    })
 }
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,6 @@
 export const SIGN_IN = 'SIGN_IN'
 export const SIGN_OUT = 'SIGN_OUT'
+export const CREATE_STREAM = 'CREATE_STREAM'
 
 /*
 Doing this so that in the event of errors, you get back an error msg.
