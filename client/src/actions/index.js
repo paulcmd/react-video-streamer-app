06984cf131010c4266e0d20230449cd0829eeb78
Paul <choxmail.pm@gmail.com>
2021-07-09 20:59:05 -0700

    added fetchStreams to fetch all streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types'
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types'
 import streams from '../apis/streams'
 
 export const signIn = (userId) => {
@@ -14,12 +22,20 @@ export const signOut = () => {
     }
 }
 
-
-export const createStream = formValues => async dispatch => {
+export const createStream = (formValues) => async (dispatch) => {
     const response = await streams.post('/streams', formValues)
-    console.log(response.data)
+    //console.log(response.data)
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
     })
 }
+
+export const fetchStreams = async dispatch => {
+    const response = await streams.get('/streams')
+    console.log(response.data)
+}
+
+/*
+ALL http axios methods - Video 340 - Bulk Action Creators 1:30
+*/
