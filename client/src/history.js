5a283f09e81c2d5432a399741a0d2397330a2df6
Paul <choxmail.pm@gmail.com>
2021-07-16 22:31:17 -0700

    Create history.js

---
+++ client/src/history.js
@@ -0,0 +1,18 @@
+import { createBrowserHistory } from 'history'; 
+export default createBrowserHistory();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+- the history package was installed with react-router-dom
+*/
