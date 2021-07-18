cce27a047376ac85a512259c7b490c75dddcb612
Paul <choxmail.pm@gmail.com>
2021-07-18 00:31:22 -0700

    created link to stream edit

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    console.log(props)
     return (
         <div>StreamEdit</div>
     )
 }
 
 export default StreamEdit
+
+/*
+- the props are coming from the Route component, from react-router-dom
+*/
