40d6f80df21d942716da174d67d1e3aad026b888
Paul <choxmail.pm@gmail.com>
2021-07-25 00:06:51 -0700

    Update Modal.js

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history'
 
 const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+        <div
+            onClick={()=> history.push('/')}
+            className="ui dimmer modals visible active">
+            <div
+                onClick={(e)=> e.stopPropagation()}
+                className="ui standard modal visible active">
                 <div className="header">Delete Stream</div>
                 <div className="content">
                     Are you sure you want to delete this stream?
@@ -25,4 +30,7 @@ export default Modal
 The modal is rendered on the body element, but not on the root div, but in the modal div in
 the public html file. This avoids having to render the modal in the sreamDelete component
 because we'd have to render in all parent elements
+-on clicking parent modal div, go home. on clicking the child to main modal parent div( anywhere in
+    the modal that is not a button) we want to stop bubbling up of that click event to the 
+    parent div(ie the one thats taking us back to home)
 */
