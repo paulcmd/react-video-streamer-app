2c2e93e9445872bbbf527f0ea20f3bafe7ef50d4
Paul <choxmail.pm@gmail.com>
2021-07-30 21:02:10 -0700

    Update StreamShow.js

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,34 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 import flv from 'flv.js'
 import { useParams } from 'react-router-dom'
 
-const StreamShow = ({ fetchStream, stream }) => {
-    const { id } = useParams()
+class StreamShow extends Component {
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef()
+    }
+    // const { id } = useParams()
 
     // console.clear()
-     console.log('stream from streamShow props', stream)
+    //  console.log('stream from streamShow props', stream)
 
-    useEffect(() => {
-        fetchStream(id)
-    }, [])
-
-    if (!stream) {
-        <div>Loading...</div>
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
     }
-    const { title, description } = stream
-    return (
-        <div>
-            <video />
-            <h1>{title}</h1>
-            <h3>{description}</h3>
-        </div>
-    )
+
+    render() {
+        if (!stream) {
+            ;<div>Loading...</div>
+        }
+        const { title, description } = stream
+        return (
+            <div>
+                <video />
+                <h1>{title}</h1>
+                <h3>{description}</h3>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    
     return {
         stream: state.streams[ownProps.match.params.id]
     }
