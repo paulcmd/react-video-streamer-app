e5fe91dc8806bff52bb717877ee17278d04fbac4
Paul <choxmail.pm@gmail.com>
2021-08-02 23:56:00 -0700

    Update StreamShow.js

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -13,12 +13,21 @@ class StreamShow extends Component {
     componentDidMount() {
         const { id } = this.props.match.params
         this.props.fetchStream(id)
-        this.player = flv.createPlayer({
-            type: 'flv',
-            url: `http://localhost:8000/live/${id}.flv`
-        })
-        this.player.attachMediaElement(this.videoRef.current)
-        this.player.load()
+        
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return
+        } {
+            const { id } = this.props.match.params
+            this.player = flv.createPlayer({
+                type: 'flv',
+                url: `http://localhost:8000/live/${id}.flv`
+            })
+            this.player.attachMediaElement(this.videoRef.current)
+            this.player.load()
+        }
     }
 
     render() {
